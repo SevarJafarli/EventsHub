456ea8429ddbf94a339724561d6bd98cb1946fd4
008458 <008458@ibar.int>
2024-06-16 15:29:18 +0400

    Initial Commit

---
+++ EventsHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsHub
+//
+//  Created by Sevar Jafarli on 16.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
